6e043310701f2f9fbb92d20ae478126900f6f879
panhaleap <panhaleap@gmail.com>
2020-04-07 08:58:43 +0700

    bind to our own properties with @Input parentheses, assign alais

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
-  @Input() element: {type: string, name: string, content: string};
+  // srvElement is alias of element
+  @Input('srvElement') element: {type: string, name: string, content: string};
   constructor() { }
 
   ngOnInit(): void {
